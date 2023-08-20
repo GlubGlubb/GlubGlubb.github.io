97be21dc181db48321700214e7cb19a5876cfbf5
GlubGlubb <142173261+GlubGlubb@users.noreply.github.com>
2023-08-20 14:01:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-var video = document.querySelector("#videoElement");
-
-if(navigator.mediaDevices.getUserMedia)
-{
-    navigator.mediaDevices.getUserMedia({video:true})
-    .then(function(stream)
-    {
-        video.srcObject = stream;
-    })
-    .catch(function(err0r)
-    {
-        console.log("Failed to get camera");
-    })
-}
+function isMobile() {
+    const isAndroid = /Android/i.test(navigator.userAgent);
+    const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    return isAndroid || isiOS;
+  }
+  
+  async function setupCamera() {
+    const isPortrait = true; // do logic here
+  
+    let video = document.getElementById('videoElement');
+  
+    console.log("Getting video");
+  
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsinline', '');
+  
+    console.log("Calling getUserMedia");
+  
+    return new Promise((resolve) => {
+      (async() => {
+        await navigator.mediaDevices.getUserMedia({
+            'audio': false,
+            'video': {
+              facingMode: 'user',
+              width: isPortrait ? 480 : 640,
+              height: isPortrait ? 640 : 480,
+            },
+          })
+          .then((stream) => {
+            console.log("Got getUserMedia stream");
+            video.srcObject = stream;
+            video.play();
+            resolve(true);
+          })
+          .catch((err) => {
+            console.log("Encountered getUserMedia error", err);
+            resolve(false);
+          });
+      })();
+    });
+  
+  }
+  
+  (async() => {
+    const ret = await setupCamera();
+    console.log(`Initialised camera: ${ret}`)
+  })();
