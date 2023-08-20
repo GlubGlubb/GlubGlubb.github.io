2ce43c56a426962a7c23e4d2dba5e4ded7d7c935
GlubGlubb <142173261+GlubGlubb@users.noreply.github.com>
2023-08-20 14:15:46 -0500

    Created VR effect

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
-function isMobile() {
-    const isAndroid = /Android/i.test(navigator.userAgent);
-    const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-    return isAndroid || isiOS;
-  }
-  console.log('s')
-  
-  async function setupCamera() {
-    const isPortrait = true; // do logic here
-  
-    let video = document.getElementById('videoElement');
-  
-    console.log("Getting video");
-  
-    video.setAttribute('autoplay', '');
-    video.setAttribute('muted', '');
-    video.setAttribute('playsinline', '');
-  
-    console.log("Calling getUserMedia");
-  
-    return new Promise((resolve) => {
-      (async() => {
-        await navigator.mediaDevices.getUserMedia({
-            'audio': false,
-            'video': {
-              facingMode: 'user',
-              width: isPortrait ? 480 : 640,
-              height: isPortrait ? 640 : 480,
-            },
-          })
-          .then((stream) => {
-            console.log("Got getUserMedia stream");
-            video.srcObject = stream;
-            video.play();
-            resolve(true);
-          })
-          .catch((err) => {
-            console.log("Encountered getUserMedia error", err);
-            resolve(false);
-          });
-      })();
+var video1 = document.querySelector("#videoElement1");
+var video2 = document.querySelector("#videoElement2");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ audio: false, 'video':{
+    facingMode:"environment"
+  } })
+    .then(function (stream) {
+      video1.srcObject = stream;
+      video2.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Failed to get camera");
     });
-  
-  }
-  
-  (async() => {
-    const ret = await setupCamera();
-    console.log(`Initialised camera: ${ret}`)
-  })();
+}
